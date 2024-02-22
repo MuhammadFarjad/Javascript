404944e7aa1e5e363582d86794adbe984e465a0e
MuhammadFarjad <154513435+MuhammadFarjad@users.noreply.github.com>
2024-02-22 18:00:30 +0500

    chap 1 done

---
+++ chap1/app.js
@@ -0,0 +1,12 @@
+
+
+alert('Welcome')
+
+alert('Error! Please enter a valid password.')
+
+alert('Welcome to JS Land... \n Happy Coding!')
+
+alert('hello... I can run JS through my web browsers console')
+
+
+
---
+++ chap2/app.js
@@ -0,0 +1,15 @@
+var username = 'Muhammad_Farjad';
+var message = 'Hello_World';
+alert(message)
+
+var name='name:Ali'
+var father_name='father name:Fayyaz'
+var age='age:17'
+
+
+alert(name)
+
+alert(father_name)
+
+
+alert(age)
