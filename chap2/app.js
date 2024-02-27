6f3bebe7a2727e2069b475618c79a4ec3054c9cb
MuhammadFarjad <154513435+MuhammadFarjad@users.noreply.github.com>
2024-02-27 15:51:47 +0500

    chap and 2

--- chap2/app.js
+++ chap2/app.js
@@ -1,15 +1,21 @@
 var username = 'Muhammad_Farjad';
 var message = 'Hello_World';
+
+var name='Farjad'
+var age='17 years old'
+// var father_name='father name:Fayyaz'
+var email='My email address is farjad5395@gmail.com'
+var book='A smarter way to learn JavaScript'
+
 alert(message)
 
-var name='name:Ali'
-var father_name='father name:Fayyaz'
-var age='age:17'
-
-
 alert(name)
-
-alert(father_name)
-
-
 alert(age)
+alert('Certified mobile application development')
+alert(email)
+alert(book)
+
+alert('“▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬”')
+// alert(father_name)
+
+
---
+++ chap3/app.js
@@ -0,0 +1,11 @@
+var age='My age is 17 years'
+
+
+
+
+
+
+
+
+
+alert(age)
